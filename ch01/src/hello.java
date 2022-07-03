412bd90aaf7ac653c4495590022650ac46786321
codinginfant <codinginfant@gmail.com>
2022-07-03 11:04:54 +0900

    ch01 hello java

---
+++ ch01/src/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");  // 화면에 글자 출력
+
+        /*
+        여러줄 주석 처리는
+        이렇게
+         */
+    }
+}
