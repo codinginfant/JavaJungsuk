281104db270b3ff11568b1213ea7ff507b11bcbc
codinginfant <107476079+codinginfant@users.noreply.github.com>
2022-07-03 12:22:17 +0900

    Add files via upload

--- ch02/src/PrintfEx1.java
+++ ch02/src/PrintfEx1.java
@@ -12,16 +12,16 @@ public class PrintfEx1 {
         int hexNum = 0x10;              // 16진수 10, 10진수로는 16
         int binNum = 0b10;              // 2진수 10, 10진수로는 2
 
-        System.out.printf("b=%d%n", b);
-        System.out.printf("s=%d%n", s);
-        System.out.printf("c=%c, %d %n", c, (int)c);
-        System.out.printf("finger=[%5d]%n", finger);
-        System.out.printf("finger=[%-5d]%n", finger);
-        System.out.printf("finger=[%05d]%n", finger);
-        System.out.printf("big=%d%n", big);
-        System.out.printf("hex=%#x%n", hex);            // # 은 접두사 (16진수 0x, 8진수 0)
-        System.out.printf("octNum=%o, %d%n", octNum, octNum);
-        System.out.printf("hexNum=%x, %d%n", hexNum, hexNum);
-        System.out.printf("bihNum=%s, %d%n", Integer.toBinaryString(binNum), binNum);
+        System.out.printf("b=%d%n", b);                // b=1      %n은 printf시 줄바꿈
+        System.out.printf("s=%d%n", s);                // s=2      %d는 10진 정수 출력
+        System.out.printf("c=%c, %d %n", c, (int)c);   // c=A, 65  %c는 문자로 출력
+        System.out.printf("finger=[%5d]%n", finger);   // finger=[   10]
+        System.out.printf("finger=[%-5d]%n", finger);  // finger=[10   ]
+        System.out.printf("finger=[%05d]%n", finger);  // finger=[00010]
+        System.out.printf("big=%d%n", big);            // big=100000000000
+        System.out.printf("hex=%#x%n", hex);           // # 은 접두사 (16진수 0x, 8진수 0)  // hex=0xffffffffffffff
+        System.out.printf("octNum=%o, %d%n", octNum, octNum);                               // octNum=10, 8
+        System.out.printf("hexNum=%x, %d%n", hexNum, hexNum);                               // hexNum=10, 16
+        System.out.printf("bihNum=%s, %d%n", Integer.toBinaryString(binNum), binNum);       // binNum=10, 2
     }
 }
